e5d369e37211c8f98595700d1c0bab7b71d10fd7
Kaleem MOHAMMAD <kmohamma@c4r10s4.wethinkcode.co.za>
2019-06-13 10:58:04 +0200

    adding and updating

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kmohamma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 13:44:59 by kmohamma          #+#    #+#             */
-/*   Updated: 2019/06/10 16:08:45 by kmohamma         ###   ########.fr       */
+/*   Updated: 2019/06/13 09:08:52 by kmohamma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	t1 = (unsigned const char *)s1;
 	t2 = (unsigned const char *)s2;
-	if (t1 == NULL || t2 == NULL)
-		return ('\0');
 	while (n)
 	{
 		if (*t1 != *t2)
